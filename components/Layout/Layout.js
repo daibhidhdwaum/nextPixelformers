9ea6dbf891d5baeadc603e4f9cf8cf4fafb22808
David Downie <daviddownie@gmail.com>
2022-01-10 21:06:16 -0500

    Add unstyled layout

---
+++ components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <div>
+        <header>Header</header>
+        <main>Main content</main>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default Layout;
